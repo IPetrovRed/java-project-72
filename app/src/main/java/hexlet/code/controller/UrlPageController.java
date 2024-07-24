a73e0066f995820cd806e8ef34976756562c752c
IPetrovRed <ipetrovdat@gmail.com>
2024-07-24 13:07:48 +0300

    controller code pack added tp proj

---
+++ app/src/main/java/hexlet/code/controller/UrlPageController.java
@@ -0,0 +1,68 @@
+package hexlet.code.controller;
+
+import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.dto.UrlPage;
+import hexlet.code.repository.UrlRepository;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.util.Routes;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.List;
+
+public class UrlPageController {
+
+    public static void showUrlPage(Context ctx) throws SQLException {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+        Url url = UrlRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Url with id: " + id + " not found"));
+        List<UrlCheck> urlChecks = UrlCheckRepository.findByUrlId(id);
+        UrlPage page = new UrlPage(url, urlChecks);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+        ctx.render("urls/urlpage.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void urlCheck(Context ctx) throws SQLException {
+        long id = ctx.pathParamAsClass("id", Long.class).getOrDefault(null);
+        Url url = UrlRepository.findById(id)
+                .orElseThrow(() -> new NotFoundResponse("Url with id: " + id + " not found"));
+
+        try {
+            HttpResponse<String> response = Unirest.get(url.getName()).asString();
+            Document doc = Jsoup.parse(response.getBody());
+            int statusCode = response.getStatus();
+            Timestamp dateandtime = new Timestamp(System.currentTimeMillis());
+            String title = doc.title();
+            Element h1El = doc.selectFirst("h1");
+            String h1 = h1El == null ? "" : h1El.text();
+            Element descr = doc.selectFirst("meta[name=description]");
+            String description = descr == null ? "" : descr.attr("content");
+            UrlCheck newCheck = new UrlCheck(statusCode, title, h1, description);
+
+            newCheck.setUrlId(id);
+            newCheck.setCreatedAt(dateandtime);
+            UrlCheckRepository.saveUrlCheck(newCheck);
+            ctx.sessionAttribute("flash", "Страница успешно проверена");
+            ctx.sessionAttribute("flash-type", "success");
+        } catch (UnirestException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flash-type", "danger");
+        } catch (Exception e) {
+            ctx.sessionAttribute("flash", e.getMessage());
+            ctx.sessionAttribute("flash-type", "danger");
+        }
+        ctx.redirect(Routes.urlPath(id));
+    }
+}
---
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -0,0 +1,64 @@
+package hexlet.code.controller;
+import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.dto.UrlsPage;
+import hexlet.code.repository.UrlRepository;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.util.Routes;
+import io.javalin.http.Context;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+
+public class UrlsController {
+    public static void createUrl(Context ctx) throws SQLException {
+        var inputUrl = ctx.formParam("url");
+        URL parsedUrl;
+        try {
+            parsedUrl = new URI(inputUrl).toURL();
+        } catch (URISyntaxException | IllegalArgumentException | NullPointerException | MalformedURLException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flash-type", "danger");
+            ctx.redirect(Routes.mainPath());
+            return;
+        }
+        String normalizedUrl = String
+                .format(
+                        "%s://%s%s",
+                        parsedUrl.getProtocol(),
+                        parsedUrl.getHost(),
+                        parsedUrl.getPort() == -1 ? "" : ":" + parsedUrl.getPort()
+                )
+                .toLowerCase();
+
+        Url url = UrlRepository.findByName(normalizedUrl).orElse(null);
+
+        if (url != null) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("flash-type", "warning");
+        } else {
+            Url newUrl = new Url(normalizedUrl);
+            UrlRepository.save(newUrl);
+            ctx.sessionAttribute("flash", "Страница успешно добавлена");
+            ctx.sessionAttribute("flash-type", "success");
+        }
+        ctx.redirect(Routes.urlsPath());
+    }
+
+    public static void showUrls(Context ctx) throws SQLException {
+        List<Url> urls = UrlRepository.getEntities();
+        Map<Long, UrlCheck> latestChecks = UrlCheckRepository.findLatestChecks();
+        UrlsPage page = new UrlsPage(urls, latestChecks);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+        ctx.render("urls/urlsindex.jte", Collections.singletonMap("page", page));
+    }
+
+}
