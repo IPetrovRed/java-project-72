b5829f20bca52d53f95c1b8d21b57a7ee0cfd2c3
IPetrovRed <ipetrovdat@gmail.com>
2024-07-30 13:12:07 +0300

    working proj without LocalDate changes

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -3,7 +3,7 @@ package hexlet.code.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import java.time.LocalDateTime;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class Url {
     private Long id;
     private String name;
-    private LocalDateTime createdAt;
+    private Timestamp createdAt;
 
     public Url(String name) {
         this.name = name;
